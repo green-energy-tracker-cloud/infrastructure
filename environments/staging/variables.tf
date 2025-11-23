88e5daa0bfd79baad68af630e65633e078821f2d
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-11-23 15:52:36 +0100

    Add variables.tf for staging environment

--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -169,3 +169,11 @@ variable "cd-pipeline-name" {
   description = "Pipeline Name"
   type = string
 }
+
+variable "secrets" {
+  description = "List of Secrets"
+  type = list(object({
+    secret_id = string
+    project = string
+  }))
+}
