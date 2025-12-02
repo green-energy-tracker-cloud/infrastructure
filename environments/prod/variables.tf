575e58156f0c564af4611375cfca926c92429043
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-12-02 15:17:36 +0100

    add storage bucket

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -178,3 +178,12 @@ variable "secrets" {
     project = string
   }))
 }
+variable "buckets" {
+  description = "List of Cloud Storage Buckets"
+  type = list(object({
+    name = string
+    location = string
+    uniform_bucket_level_access = bool
+    force_destroy = bool
+  }))
+}
