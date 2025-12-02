84a2969cec114566f806fda9b2a1390175567465
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-12-02 14:00:18 +0100

    remove storage bucket

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -178,13 +178,3 @@ variable "secrets" {
     project = string
   }))
 }
-
-variable "buckets" {
-  description = "List of Cloud Storage Buckets"
-  type = list(object({
-    name = string
-    location = string
-    uniform_bucket_level_access = bool
-    force_destroy = bool
-  }))
-}
