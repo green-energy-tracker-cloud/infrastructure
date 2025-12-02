7dde2bcd5f41820f2afdb4812a895f1e0473af7a
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-12-02 15:34:35 +0100

    add storage bucket

--- modules/03 - storage/cloudstorage/main.tf
+++ modules/03 - storage/cloudstorage/main.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "deploy_artifacts" {
   for_each = {
     for buckets in var.buckets : buckets.name => buckets
   }
-  name = "deploy-artifacts-${each.key}"
+  name = each.key
   location = each.value.location
   uniform_bucket_level_access = each.value.uniform_bucket_level_access
   force_destroy = each.value.force_destroy
