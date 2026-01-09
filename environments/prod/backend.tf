035a048e6a79e5c1928c551df5734b28d73189fc
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2026-01-09 10:45:22 +0100

    add Container Scanning API

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -3,6 +3,6 @@ terraform {
   backend "gcs" {
     # Specifies the Google Cloud Storage bucket where the 'terraform.tfstate' file will be stored.
     # This enables remote state management, collaboration, and state locking.
-    bucket = "green-energy-tracker-cloud-tfstate"
+    bucket = "green-energy-tracker-cloud-v3-tfstate"
   }
 }
--- modules/04 - sdlc/clouddeploy/pipeline/main.tf
+++ modules/04 - sdlc/clouddeploy/pipeline/main.tf
@@ -2,7 +2,7 @@ resource "google_clouddeploy_delivery_pipeline" "primary" {
   location    = "us-east1"
   name        = "delivery-pipeline"
   description = "Pipeline with performance tests and Canary rollout"
-  project     = "reference-lens-482009-j5"
+  project     = "green-energy-tracker-cloud-v3"
   provider    = google-beta
 
   # --- Serial Pipeline ---
